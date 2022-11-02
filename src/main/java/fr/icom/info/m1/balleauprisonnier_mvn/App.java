11a96eb28b3d132be499a7eb929774275678385e
alshizzar <dekkerseiko1220@gmail.com>
2022-11-02 15:12:05 +0100

    test n 560

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/App.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/App.java
@@ -17,6 +17,7 @@ public class App extends Application
 	 * En javafx start() lance l'application
 	 * On cree le SceneGraph de l'application ici
 	 * @see "http://docs.oracle.com/javafx/2/scenegraph/jfxpub-scenegraph.htm"
+	 *jndjfknjlsdlqKHKLQGLKDKLESOIYVBOZHTOIMHZRIQ  Y TYQHEZKRHTTP%ORJEZOQIJT
 	 */
 	@Override
 	public void start(Stage stage) throws Exception 
